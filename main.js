a1ccbc604897070507d7be13635e6ef063dd55e0
Mahad444 <mahadsaid95@icloud.com>
2022-10-25 12:54:12 +0300

    practicing conditional statements

---
+++ main.js
@@ -0,0 +1,24 @@
+// IF STATEMENTS
+num = null
+
+if (num > 0){
+    console.log("Number is Greater than zero")
+}else if (num < 0){
+    console.log("Number is less than zero")
+}else{
+    console.log("Number is zero")
+}
+
+let color =""
+
+switch (color){
+    case "red" :
+    console.log("red")
+    break;
+    case "blue":console.log("blue")
+    break;
+    case 'yellow':console.log("Yellow")
+    break;
+    default :
+    console.log("Not Valid Color")
+}
